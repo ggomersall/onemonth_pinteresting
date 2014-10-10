d55ca01778667296f077a05515bad8930bcf70a1
ggomersall <ggomersall@gmail.com>
2014-10-10 10:21:37 +0100

    pins controller removed respond_with added redirect_to

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,16 +3,16 @@ class PinsController < ApplicationController
 
   def index
     @pins = Pin.all
-    respond_with (@pins)
+   
   end
 
   def show
-    respond_with(@pin)
+    
   end
 
   def new
     @pin = Pin.new
-    respond_with(@pin)
+    
   end
 
   def edit
@@ -21,17 +21,20 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     @pin.save
-    respond_with(@pin)
+    redirect_to pins_path
+    
   end
 
   def update
     @pin.update(pin_params)
-    respond_with(@pin)
+    redirect_to pins_path
+    
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to pins_path
+    
   end
 
   private
